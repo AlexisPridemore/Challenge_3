3908e0eeb042894d6d739c80471a84e1cd6755c0
Alexis Pridemore <alpridemore@usf.edu>
2025-02-02 21:33:57 -0500

    Task 2-Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -7,3 +7,14 @@ prices.push(30);     //Add a new price to the array
 prices.shift();      //Remove the first price from the array
 
 console.log("Updated Price List:", prices);   
+
+// Task 2 Modifying Customer Orders Scenario: Order List You are processing customer orders and need to update order quantities.
+
+let orders = ["5", "10", "15", "20", "25"];    //Declare an array orders
+
+orders.splice(2, 1, "20");   //Increase third order quantity by 5
+
+let totalOrders = orders.reduce((total, quantity) => total +quantity, 0);   // Calculate total of all orders
+
+console.log("Updated Orders Array:", orders); 
+console.log("Total Orders Count:", totalOrders);    
