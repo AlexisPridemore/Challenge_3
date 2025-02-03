e187f8f70e84e2ef3f99a07edeaf0c4533eab383
Alexis Pridemore <alpridemore@usf.edu>
2025-02-02 21:33:17 -0500

    Task 1- Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,9 @@
+// Task 1 Product Price Management Scenario: Online Store You are managing a product pricing system.
+
+let prices = ["20", "40", "60", "80", "100"];    //Declare an array prices 
+
+prices.push(30);     //Add a new price to the array
+
+prices.shift();      //Remove the first price from the array
+
+console.log("Updated Price List:", prices);   
